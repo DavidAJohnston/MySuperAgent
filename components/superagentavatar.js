bdb4852eb3a8fabf46ecac4a689e22727c9e057c
David Johnston <DJohnstonEC@gmail.com>
2025-03-04 15:53:37 -0600

    Create superagentavatar.js
    v3

---
+++ components/superagentavatar.js
@@ -0,0 +1,10 @@
+export default function SuperAgentAvatar({ className }) {
+  return (
+    <img
+  
+      src="/   "
+      alt="Super Agent Avatar"
+      className={`h-48 w-48 object-cover rounded-full ${className || ''}`}
+    />
+  );
+}
