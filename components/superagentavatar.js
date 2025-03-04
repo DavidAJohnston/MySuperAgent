01e175fc93f26f570e730368985545f32ba0127c
David Johnston <DJohnstonEC@gmail.com>
2025-03-04 16:04:39 -0600

    Update superagentavatar.js
    v3

--- components/superagentavatar.js
+++ components/superagentavatar.js
@@ -1,7 +1,7 @@
 export default function SuperAgentAvatar({ className }) {
   return (
     <img
-      src="  "
+      src="/robot-avatar.png"
       alt="Super Agent Avatar"
       className={`h-48 w-48 object-cover rounded-full ${className || ''}`}
     />
