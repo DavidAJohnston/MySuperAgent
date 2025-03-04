be157779bd114fe3efe973bc50867e84cccd3329
David Johnston <DJohnstonEC@gmail.com>
2025-03-04 16:59:26 -0600

    Delete components directory

--- components/superagentavatar.js
+++
@@ -1,9 +0,0 @@
-export default function SuperAgentAvatar({ className }) {
-  return (
-    <img
-      src="/robot-avatar.png"
-      alt="Super Agent Avatar"
-      className={`h-48 w-48 object-cover rounded-full ${className || ''}`}
-    />
-  );
-}
